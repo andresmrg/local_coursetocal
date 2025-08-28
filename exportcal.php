e75eced6a4b0df7303d05f3aea6c57be0f97bfc2
Emma Richardson <emmar@ecboces.org>
2025-08-28 08:50:00 -0600

    Update exportcal.php
    Fix ics not generating

--- exportcal.php
+++ exportcal.php
@@ -1,41 +1,33 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Confirm self registered user.
- *
- * @package    local_coursetocal
- * @copyright  2020 LMS Doctor
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
 require(__DIR__ . '/../../config.php');
 
 global $CFG, $USER, $DB;
 
-$password = $DB->get_record('user', array('id' => $USER->id), 'password');
-$params = array();
-$params['userid']      = $USER->id;
-$params['authtoken']   = sha1($USER->id . (isset($password->password) ? $password->password : '') . $CFG->calendar_exportsalt);
-$params['eventid']   = optional_param('eventid', 0, PARAM_INT);
+// Must be logged in to generate a token-bound export.
+require_login();
 
-$link = new moodle_url('/local/coursetocal/export.php', $params);
-$urlclasses = array('class' => 'generalbox calendarurl');
-$calendarurl = html_writer::tag(
-    'div',
-    get_string('calendarurl', 'local_coursetocal', $link->out()),
-    $urlclasses
-);
+// Fetch current user's password hash (for the token).
+$rec = $DB->get_record('user', ['id' => $USER->id], 'id, password', MUST_EXIST);
+
+// Required param from the link.
+$eventid = optional_param('eventid', 0, PARAM_INT);
+if (empty($eventid)) {
+    print_error('missingparam', 'error', '', 'eventid');
+}
+
+// Token must use the site’s calendar export salt.
+if (empty($CFG->calendar_exportsalt)) {
+    print_error('generalexceptionmessage', 'error', '', 'Calendar export salt is not set in admin settings.');
+}
+
+$params = [
+    'userid'    => $USER->id,
+    'authtoken' => sha1($USER->id . $rec->password . $CFG->calendar_exportsalt),
+    'eventid'   => $eventid,
+];
+
+// Optional debug line while testing (check web server error log):
+// error_log('coursetocal redirect -> ' . (new moodle_url('/local/coursetocal/export.php', $params))->out(false));
+
+// Send browser to the actual exporter (which should emit the ICS).
+redirect(new moodle_url('/local/coursetocal/export.php', $params));
